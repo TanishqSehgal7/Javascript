498462c1eebd2085a3cb6c44066daae1d4b3ac97
TanishqSehgal7 <tsgl7246@gmail.com>
2024-01-07 21:28:54 +0530

    Type Comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,47 @@
+console.log(3>4)
+console.log(3>=4)
+console.log(3<4)
+console.log(3<=4)
+console.log(3==4)
+console.log(3!=4)
+
+console.log("Comparison of different datatype values")
+console.log("2" > 1)
+console.log(2 > "1")
+console.log("2" > "1")
+console.log("02" > "1")
+console.log("02" > "01")
+console.log("02" > 1)
+console.log(2 > "01")
+
+console.log("Null comparison")
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null > 0)
+console.log(null != 0)
+
+console.log("Undefined Comparison")
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined != 0)
+
+/* There are inconsistencies in datatype conversion when 
+2 different datatypes are being compared. Hence, such type
+of comparisons should be avoided. */ 
+
+//  === strict check: check both the value and the datatype
+console.log("Strict Check")
+console.log(1 === 1)
+console.log(1 === "1")
+
+
+
+
+
+
+
+
+
