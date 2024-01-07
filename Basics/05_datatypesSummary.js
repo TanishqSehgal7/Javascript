12fb265349d81c15254e6fb4e3bdce9baa7c8c9d
TanishqSehgal7 <tsgl7246@gmail.com>
2024-01-07 23:43:32 +0530

    DataTypes Summary

---
+++ Basics/05_datatypesSummary.js
@@ -0,0 +1,27 @@
+/* 
+Datatypes are Primitive and Derived/Referenced
+All primitive datatypes are call by value type, ie a copy
+of the values is given for usage but not the original 
+memory reference of the values.
+
+7 Types:
+String, Number, Boolean, null, undefined, 
+symbol (for unique values), BigInt
+
+Referenced or Derived or Non Primitive Datatypes: the original
+memory reference of these datatypes can be directly allocated
+for usage
+
+Types:
+Array, Object, Functions
+
+Java script is a dynamically typed language which means 
+the type is associated with run time values and not with
+named variables/fields etc. While a statically typed language
+is one in which the the type of variable is known at compile
+time
+
+
+
+*/
+
