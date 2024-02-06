ca168579a055307ccee45bb02b0c295ec4b24a78
TanishqSehgal7 <tsgl7246@gmail.com>
2024-02-06 23:52:11 +0530

    Promise in JS

--- 11_Promise/01_promise.js
+++ 11_Promise/01_promise.js
@@ -1,7 +1,17 @@
 /*
 Promise object represents the eventual completion of faliure
 of an asynchronous operation and its resulting value.
-
-
-
 */
+
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an async task
+    // DB calls, cryptography, Network calls
+    setTimeout(function(){
+        console.log("Async task is complete")
+        resolve()
+    })
+})
+
+promiseOne.then(function(){
+    console.log("Promise Consumed!")
+})
